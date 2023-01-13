b9f14596c4f52b7709d985d68e746ed14e529e26
jackniu <jackniu@yeahka.com>
2023-01-13 17:24:32 +0800

    update init script add service

--- tools/init.sh
+++ tools/init.sh
@@ -1,5 +1,5 @@
 # !/bin/sh
-command -v kratos >/dev/null 2>&1 || { echo >&2 "command kratos not found"; exit 1; }
+command -v protoc >/dev/null 2>&1 || { echo >&2 "command kratos not found"; exit 1; }
 CURDIR=$(pwd)
 RUNDIR=$(dirname $CURDIR)
 echo "rundir:${RUNDIR}"
@@ -10,16 +10,18 @@ function PrintHelp()
     echo -e "\tOption:"
     echo -e "\t -n \t project name"
     echo -e "\t -p \t proto name"
+    echo -e "\t -s \t service name"
     echo -e "\t -c \t path of config file"
     exit 1
 }
 
-while getopts 'p:c:n:h' OPT; do
+while getopts 'p:c:n:s:h' OPT; do
     case $OPT in
         h) PrintHelp;;
         n) PROJECT="$OPTARG";PROTOBASE=$PROJECT;;
         p) PROTOBASE="$OPTARG";;
         c) CPATH="$OPTARG";;
+        s) SERVICE="$OPTARG";;
         ?) PrintHelp;;
     esac
 done
@@ -47,7 +49,20 @@ for i in `grep "layout" -r --exclude-dir=local --exclude-dir=.git --exclude=*.ex
 do
     sed -i "s/layout/${PROJECT}/g" $i
 done
-kratos proto client api/
+
+if [ -n "$SERVICE" ];then
+    for i in `grep -E "[gG]reeter" -r --exclude-dir=local --exclude-dir=.git --exclude=*.exe --exclude=*.sh | cut -d: -f1 | uniq`
+    do
+        Upper=$(echo ${SERVICE^})
+        Lower=$(echo ${SERVICE,})
+        sed -i "s/Greeter/${Upper}/g" $i
+        sed -i "s/greeter/${Lower}/g" $i
+    done
+
+
+fi
+
+make api
 
 if [ ! -n "$CPATH" ];then
     CPATH=local/dev/config.yaml
